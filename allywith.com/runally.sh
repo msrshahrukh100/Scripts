414372d83ea858e1d0625160b2e8499ee95cbc52
Shahrukh <msr.concordfly@gmail.com>
2020-01-01 18:45:53 +0530

    Added few more config

---
+++ allywith.com/runally.sh
@@ -0,0 +1,5 @@
+cd /home/shahrukh/Code/projects/allywith
+. env/bin/activate
+source .shahrukh_aliases
+cd Codeilm.com
+python manage.py runserver
--- changewallpaper/changewallpapers.sh
+++ changewallpaper/changewallpapers.sh
@@ -1,3 +1,5 @@
 #!/bin/sh
 
-python3 /home/shahrukh/Code/scripts/changewallpaper/changewallpapers.py
+output=$(python3 /home/shahrukh/Code/scripts/changewallpaper/changewallpapers.py 2>&1)
+echo $output
+gsettings set org.gnome.desktop.background picture-uri file:///home/shahrukh/Desktop/Wallpapers/$output > /home/shahrukh/Desktop/cron.log 2>&1
